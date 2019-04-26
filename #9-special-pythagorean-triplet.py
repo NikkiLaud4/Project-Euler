08ef36278dec97e0045a1af995697f0ac589261e
NikkiLaud4 <49675363+NikkiLaud4@users.noreply.github.com>
2019-04-26 19:50:04 +0530

    Add files via upload

---
+++ #9-special-pythagorean-triplet.py
@@ -0,0 +1,12 @@
+
+def pyth(a,b):
+    c=1000-(a+b)
+    if((c**2)==(a**2)+(b**2)):
+        {
+            print("Values:",a,b,c,"\nSum:",a+b+c,"\nProduct:",a*b*c)
+            
+        }
+        
+for i in range(1,1000):
+    for j in range(999,1,-1):
+        f = pyth(i,j);
